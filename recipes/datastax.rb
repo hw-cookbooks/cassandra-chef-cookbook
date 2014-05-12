7d69b542353de00c45aec41c8895c058eba753b9
Cameron Johnston <cameron@rootdown.net>
2014-05-11 22:55:22 -0600

    extend datastax recipe to support installation of enterprise edition on rhel platforms

--- recipes/datastax.rb
+++ recipes/datastax.rb
@@ -20,6 +20,8 @@
 # This recipe relies on a PPA package and is Ubuntu/Debian specific. Please
 # keep this in mind.
 
+::Chef::Recipe.send(:require, 'uri')
+
 include_recipe "java"
 
 user node.cassandra.user do
@@ -44,49 +46,64 @@ end
   end
 end
 
-case node["platform_family"]
-when "debian"
-  if node['cassandra']['dse']
-    dse = node.cassandra.dse
-    if dse.credentials.databag
-      dse_credentials = Chef::EncryptedDataBagItem.load(dse.credentials.databag.name, dse.credentials.databag.item)[dse.credentials.databag.entry]
-    else
-      dse_credentials = dse.credentials
-    end
-
-    package "apt-transport-https"
-
-    apt_repository "datastax" do
-      uri          "http://#{dse_credentials['username']}:#{dse_credentials['password']}@debian.datastax.com/enterprise"
-      distribution "stable"
-      components   ["main"]
-      key          "http://debian.datastax.com/debian/repo_key"
-      action :add
-    end
+# collect credentials for downloading enterprise edition, if present
+if node['cassandra']['dse']
+  dse = node.cassandra.dse
+  if dse.credentials.databag
+    dse_credentials = Chef::EncryptedDataBagItem.load(dse.credentials.databag.name,dse.credentials.databag.item)[dse.credentials.databag.entry]
   else
-    apt_repository "datastax" do
-      uri          "http://debian.datastax.com/community"
-      distribution "stable"
-      components   ["main"]
-      key          "http://debian.datastax.com/debian/repo_key"
+    dse_credentials = dse.credentials
+  end
+end
 
-      action :add
-    end
+# generate platform-specific repo URI
+repo_host = value_for_platform(['debian','ubuntu'] => {
+                                 'default' => 'debian.datastax.com'
+                               },
+                               [ 'rhel', 'centos', 'amazon' ] => {
+                                 'default' => 'rpm.datastax.com'
+                               })
 
-    # DataStax Server Community Edition package will not install w/o this
-    # one installed. MK.
-    package "python-cql" do
+# set the repo URI path and credentials, depending on
+# the availability of enterprise edition credentials
+repo_path = dse_credentials ? '/enterprise' : '/community'
+
+repo_config = { :host => repo_host, :path => repo_path }
+
+if dse_credentials
+  repo_user = dse_credentials['username']
+  repo_pass = dse_credentials['password']
+  repo_config.merge!({ :userinfo => [repo_user, repo_pass].join(':') })
+end
+
+repo_uri = URI::HTTPS.build(repo_config)
+
+case node.platform_family
+when "debian"
+  package "apt-transport-https"
+
+  apt_repository "datastax" do
+    uri          repo_uri.to_s
+    distribution "stable"
+    components   ["main"]
+    key          "http://debian.datastax.com/debian/repo_key"
+    action       :add
+  end
+
+  # DataStax Server Community Edition package will not install w/o this
+  # one installed. MK.
+  package "python-cql" do
+    action :install
+    only_if { repo_uri.path == '/community' }
+  end
+
+  # This is necessary because apt gets very confused by the fact that the
+  # latest package available for cassandra is 2.x while you're trying to
+  # install dsc12 which requests 1.2.x.
+  if node.platform_family == "debian" then
+    package "cassandra" do
       action :install
-    end
-
-    # This is necessary because apt gets very confused by the fact that the
-    # latest package available for cassandra is 2.x while you're trying to
-    # install dsc12 which requests 1.2.x.
-    if node[:platform_family] == "debian" then
-      package "cassandra" do
-        action :install
-        version node.cassandra.version
-      end
+      version node.cassandra.version
     end
   end
 
@@ -95,7 +112,7 @@ when "rhel"
 
   yum_repository "datastax" do
     description "DataStax Repo for Apache Cassandra"
-    baseurl "http://rpm.datastax.com/community"
+    baseurl repo_uri.to_s
     gpgcheck false
     action :create
   end
