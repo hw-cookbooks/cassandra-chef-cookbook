7d3f6b5f7dec82e9cb3f4addf1ae19875dc1a392
Cameron Johnston <cameron@rootdown.net>
2014-05-15 16:03:31 -0600

    remove repo resource duplication in datastax and opscenter recipes

--- recipes/datastax.rb
+++ recipes/datastax.rb
@@ -20,8 +20,7 @@
 # This recipe relies on a PPA package and is Ubuntu/Debian specific. Please
 # keep this in mind.
 
-::Chef::Recipe.send(:require, 'uri')
-
+include_recipe "cassandra::_datastax_repo"
 include_recipe "java"
 
 user node.cassandra.user do
@@ -46,50 +45,8 @@ end
   end
 end
 
-# collect credentials for downloading enterprise edition, if present
-if node['cassandra']['dse']
-  dse = node.cassandra.dse
-  if dse.credentials.databag
-    dse_credentials = Chef::EncryptedDataBagItem.load(dse.credentials.databag.name,dse.credentials.databag.item)[dse.credentials.databag.entry]
-  else
-    dse_credentials = dse.credentials
-  end
-end
-
-# generate platform-specific repo URI
-repo_host = value_for_platform(['debian','ubuntu'] => {
-                                 'default' => 'debian.datastax.com'
-                               },
-                               [ 'rhel', 'centos', 'amazon' ] => {
-                                 'default' => 'rpm.datastax.com'
-                               })
-
-# set the repo URI path and credentials, depending on
-# the availability of enterprise edition credentials
-repo_path = dse_credentials ? '/enterprise' : '/community'
-
-repo_config = { :host => repo_host, :path => repo_path }
-
-if dse_credentials
-  repo_user = dse_credentials['username']
-  repo_pass = dse_credentials['password']
-  repo_config.merge!({ :userinfo => [repo_user, repo_pass].join(':') })
-end
-
-repo_uri = URI::HTTPS.build(repo_config)
-
 case node.platform_family
 when "debian"
-  package "apt-transport-https"
-
-  apt_repository "datastax" do
-    uri          repo_uri.to_s
-    distribution "stable"
-    components   ["main"]
-    key          "http://debian.datastax.com/debian/repo_key"
-    action       :add
-  end
-
   # DataStax Server Community Edition package will not install w/o this
   # one installed. MK.
   package "python-cql" do
@@ -106,22 +63,11 @@ when "debian"
       version node.cassandra.version
     end
   end
-
 when "rhel"
-  include_recipe "yum"
-
-  yum_repository "datastax" do
-    description "DataStax Repo for Apache Cassandra"
-    baseurl repo_uri.to_s
-    gpgcheck false
-    action :create
-  end
-
   yum_package "#{node.cassandra.package_name}" do
     version "#{node.cassandra.version}-#{node.cassandra.release}"
     allow_downgrade
   end
-
 end
 
 link '/etc/init.d/cassandra' do
--- recipes/opscenter_agent_datastax.rb
+++ recipes/opscenter_agent_datastax.rb
@@ -1,42 +1,5 @@
+include_recipe "cassandra::_datastax_repo"
 include_recipe "java"
-
-case node["platform_family"]
-when "debian"
-
-  if node['cassandra']['dse']
-    dse = node['cassandra']['dse']
-    if dse['credentials']['databag']
-      dse_credentials = Chef::EncryptedDataBagItem.load(dse['credentials']['databag']['name'], dse['credentials']['databag']['item'])[dse['credentials']['databag']['entry']]
-    else
-      dse_credentials = dse['credentials']
-    end
-    apt_repository "datastax" do
-      uri          "http://#{dse_credentials['username']}:#{dse_credentials['password']}@debian.datastax.com/enterprise"
-      distribution "stable"
-      components   ["main"]
-      key          "https://debian.datastax.com/debian/repo_key"
-      action :add
-    end
-  else
-    apt_repository "datastax" do
-      uri          "https://debian.datastax.com/community"
-      distribution "stable"
-      components   ["main"]
-      key          "https://debian.datastax.com/debian/repo_key"
-      action :add
-    end
-  end
-
-when "rhel"
-  include_recipe "yum"
-
-  yum_repository "datastax" do
-    description "DataStax Repo for Apache Cassandra"
-    baseurl "http://rpm.datastax.com/community"
-    gpgcheck false
-    action :create
-  end
-end
 
 server_ip = node[:cassandra][:opscenter][:agent][:server_host]
 if !server_ip
@@ -46,7 +9,7 @@ if !server_ip
   else
     return # Continue until opscenter will come up
   end
-end 
+end
 
 package "#{node[:cassandra][:opscenter][:agent][:package_name]}" do
   action :install
